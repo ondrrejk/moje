94129c057d58fb106e351d84bab8c30899b36d01
Ondřej <ondrejkopas@gmail.com>
2026-02-02 10:25:48 +0100

    fdasf

--- physics_formulas/main.py
+++ physics_formulas/main.py
@@ -8,10 +8,23 @@ icon = PhotoImage(file='physics_formulas/logo.png')
 window.iconphoto(True,icon)
 window.config(background="#ffffff")
 
-label1 = Label(window, text="Hello world!", font=("Arial Bold", 50))
+label1 = Label(window, text="Hello world!", font=("Arial Bold", 10))
 label1.grid(column=0, row=0)
 
 bt = Button(window, text="Click me")
 bt.grid(column=1, row=0)
+
+bt_orange = Button(window, text="Orange", fg="yellow", bg="orange")
+bt_orange.grid(column=0, row=1)
+
+txt = Entry(window, width=10)
+txt.grid(column=0, row=2)
+
+def clicked():
+    res = "Welcome to "+txt.get()
+    label1.configure(text=res)
+
+bt_clickable = Button(window, text="CLICK ME!", command=clicked)
+bt_clickable.grid(column=1, row=1)
 
 window.mainloop()
