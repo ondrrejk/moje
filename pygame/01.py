4e9ddf532edda685533ebd8a1ea027f62eaa559d
Ondřej Kopas <ondrejkopas@gmail.com>
2026-02-02 11:21:37 +0100

    Create basic Pygame window with event handling
    Initialize Pygame, set up display, and handle events.

---
+++ pygame/01.py
@@ -0,0 +1,19 @@
+pygame.init()
+screen = pygame.display.set_mode((800, 800))
+clock = pygame.time.Clock()
+current_time = 0
+button _press_time = 0
+
+while True:
+for event in pygame.event.get(): if event.type == pygame.QUIT:
+pygame.quit()
+sys.exit()
+if event. type == pygame. KEYDOWN:
+button_press_time = pygame.time.get_ticks()
+screen.fill (255,255, 255))
+current_time = pygame.time.get_ticks()
+current_time - button_press_time > 2000:
+screen.fill
+print(f"current time: {current_time} button press time: {button_press_time}")
+pygame.display.flip()
+clock.tick (60)
