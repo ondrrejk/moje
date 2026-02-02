1ed6f940c58bec30bc4dbb71463b5dda8d12efc9
Ondřej <ondrejkopas@gmail.com>
2026-02-02 08:36:55 +0100

    gsadgs

---
+++ physics_formulas/main.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+window = Tk()
+window.geometry("420x420")
+window.title("Python app 123")
+
+icon = PhotoImage(file='logo.jpg')
+window.iconphoto(True,icon)
+
+window.mainloop()
