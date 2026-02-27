5ce026166c2254a441c966ec9bd6e2c2573365f3
Ondřej <ondrejkopas@gmail.com>
2026-02-27 08:44:01 +0100

    fds

--- ai/magicmethod.py
+++ ai/magicmethod.py
@@ -7,9 +7,13 @@ class Vector:
     def __add__(self, other):
         return Vector(self.x + other.x, self.y + other.y)
     
+    def __repr__(self):
+        return f"X: {self.x}, Y: {self.y}"
+    
 v1 = Vector(10, 20)
 v2 = Vector(50, 60)
 v3 = v1 + v2
 
 print(v3.x)
 print(v3.y)
+print(v3)
