df9f40fffe13f8577456fe3905db0b26269ddf4f
Ondřej <ondrejkopas@gmail.com>
2026-02-27 08:45:34 +0100

    call

--- ai/magicmethod.py
+++ ai/magicmethod.py
@@ -10,6 +10,12 @@ class Vector:
     def __repr__(self):
         return f"X: {self.x}, Y: {self.y}"
     
+    def __len__(self):
+        return 10
+    
+    def __call__(self):
+        print("Hello, I was called!")
+    
 v1 = Vector(10, 20)
 v2 = Vector(50, 60)
 v3 = v1 + v2
@@ -17,3 +23,5 @@ v3 = v1 + v2
 print(v3.x)
 print(v3.y)
 print(v3)
+print(len(v3))
+v3()
